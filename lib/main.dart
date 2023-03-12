b7ed0d3e2de71b3ff99578b8fdd39e242d67cc27
Harsh Kandoi <harshkandoi8@gmail.com>
2023-03-12 12:34:40 +0530

    created my first flutter project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,42 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.lightBlue,
+          appBar: AppBar(
+              title: Text(
+                'Ask Me Anything',
+              ),
+              backgroundColor: Colors.blueAccent),
+          body: magicball(),
+        ),
       ),
     );
+
+class magicball extends StatefulWidget {
+  @override
+  State<magicball> createState() => _magicballState();
+}
+
+class _magicballState extends State<magicball> {
+  @override
+  int changeimge = 1;
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(children: <Widget>[
+        Expanded(
+          child: TextButton(
+              onPressed: () {
+                setState(() {
+                  changeimge = Random().nextInt(5) + 1;
+                });
+              },
+              child: Image.asset('android/images/ball$changeimge.png')),
+        ),
+      ]),
+    );
+  }
+}
